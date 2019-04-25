13c045e922b0f6416e5ec3a73635584b37f6108b
Cameron Bass <cameronlbass@csus.edu>
2019-04-24 23:21:34 -0700

    Added a bit of code, following Item 3 of the dataflow sheet.

--- CSC131_Project/src/Cellphone.java
+++ CSC131_Project/src/Cellphone.java
@@ -1,5 +1,7 @@
 public class Cellphone {
 	//Register new tags?
 	//Listen for tags
+	public void getTagInfo(ItemTag IT) {
+	}
 	//Relay tag data to server
 }
--- CSC131_Project/src/ItemTag.java
+++ CSC131_Project/src/ItemTag.java
@@ -1,5 +1,17 @@
 public class ItemTag {
+	private int ID;
+	private double GPSLat;
+	private double GPSLon;
 	//Startup
+	public ItemTag(int ID) {
+		this.ID=ID;
+		this.GPSLat = -90.0 + Math.random() * 180.0;
+		this.GPSLon = -180.0 + Math.random() * 360;
+	}
 	//Shutdown
 	//Transmit info
+	public String Transmit() {
+		return Integer.toString(ID).concat(" " + Double.toString(GPSLat).concat(" " + Double.toString(GPSLon)));
+		//Returns "<ID> <GPSLat> <GPSLon>"
+	}
 }
