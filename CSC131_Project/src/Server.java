1b1d2568fcf2809e8ac3930e6b07bf5dd213a78b
Cameron <cameronlbass@csus.edu>
2019-05-02 16:20:49 -0700

    Some Server code to set the format of the DB file.

--- CSC131_Project/src/Server.java
+++ CSC131_Project/src/Server.java
@@ -1,6 +1,49 @@
+import java.io.*;
+import java.util.*;
 public class Server {
+	public static void main(String[] args) {
+		Server s = new Server();
+		s.create("ass@butts.com",1234,"blue jacket");
+	}
+	String ServerDataFile = "db.txt";
 	// CRUD: Create, Read, Update, Delete
-	//Create: Register new tag with owner info
+	//Create: Register new tag with owner info.
+	void create(String email, int TagID) {
+		create(email, TagID, "");
+	}
+	void create(String email, int TagID, String ItemDescription) {
+		String Record = email + " " + Integer.toString(TagID) + " " + ItemDescription;
+		String Temp;
+		String[] Temps;
+		LinkedList<String> Records = new LinkedList<String>();
+		boolean TagPresent = false;
+		//Read all entries, scan for existing tag. If tag does not exist, append new record to file.
+		try {
+			System.out.println("Log: " + "Reading DB file.");
+			BufferedReader fin = new BufferedReader(new FileReader(ServerDataFile));
+			while((Temp=fin.readLine()) != null){
+				Records.add(Temp);
+				Temps = Temp.split(" ");
+				if(Temps[1] != null) TagPresent = (Math.abs(TagID) == Math.abs(Integer.parseInt(Temps[1])));
+			}
+			fin.close();
+		} catch (Exception e) {
+			System.out.println("Exception reading: " + e.getMessage());
+		}
+		if(!TagPresent) {
+			try {
+				System.out.println("Log: " + "Appending Record to DB file.");
+				BufferedWriter fout = new BufferedWriter(new FileWriter(ServerDataFile,true));
+				fout.write(Record);
+				fout.write(13);
+				fout.write(10);
+				fout.close();
+			} catch(Exception e) {
+				System.out.println("Exception writing: " + e.getMessage());
+				return;
+			}
+		}
+	}
 	//Read: Read info about a tag
 	//Update: Update tag status
 	//Delete: Delete tag from database
