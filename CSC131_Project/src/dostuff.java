e12716c362fec2be1ac6e86b5f79c3022fb6c922
Gamma <Gamma@GAMMA.mhs>
2019-05-02 17:47:05 -0700

    added some comments for direction on dostuff.java

--- CSC131_Project/src/dostuff.java
+++ CSC131_Project/src/dostuff.java
@@ -3,5 +3,12 @@ public class dostuff {
 		for(String s : args) {
 			System.out.print(s + " ");
 		}
+		
+		//users can add ids to server
+		//user can notify server an item is lost
+		//finder cellphone finds tagid using Transmit and getTagInfo
+		//use cellphone.java to notify server
+		//server checks database if valid info
+		//if valid, update database and notify owner with an email
 	}
 }
