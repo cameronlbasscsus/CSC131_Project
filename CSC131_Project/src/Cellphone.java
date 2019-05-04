d4b675ef6fa14f9473cbbdb7cec742d56711d317
Sean Foley <Sean Foley@SeanPC.attlocal.net>
2019-05-04 16:54:21 -0700

    wrong vars used in disToTag fixed

--- CSC131_Project/src/Cellphone.java
+++ CSC131_Project/src/Cellphone.java
@@ -50,7 +50,7 @@ public class Cellphone {
 		  TagLat = Math.toRadians(TagLat);
 
 	    double  a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+		          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(CallLat) * Math.cos(TagLat); 
 		double  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 		return earthRadiusKm * c;
 		
